d85c388e6a92a9d72a8ff4d7ff86452fde7b6df5
Jorge Bejar <jorge@wyeworks.com>
2013-12-12 19:01:56 -0200

    Inicial

---
+++ app/assets/javascripts/websites.js
@@ -0,0 +1,3 @@
+$(function() {
+  $("body").append("<h3>You are browsing: "+ document.location.href + "</h3>");
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Directory::Application.config.secret_key_base = 'f3e54f9725e98777bb73d962d67f421a08246a21cf70778f9471d2ad497d3430af8973ab5d4c2f508ad1e74666f652cc7227a42cd49ae1616c33fda8230f135f'
