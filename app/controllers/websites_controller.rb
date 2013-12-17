cc2a5862bb1efd7f4b7a8c7a6ae62e5b760e6c7b
Diego Acosta <diego@wyeworks.com>
2013-12-17 10:00:16 -0200

    Ejercicio 4

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,6 +2,8 @@ class WebsitesController < ApplicationController
 
   layout "consumer"
 
+  before_action :set_locate
+
   # GET /websites
   # GET /websites.json
   def index
@@ -12,5 +14,11 @@ class WebsitesController < ApplicationController
   # GET /websites/1.json
   def show
     @website = Website.find(params[:id])
+  end
+
+  private
+
+  def set_locate
+    I18n.locale = params[:locale]
   end
 end
